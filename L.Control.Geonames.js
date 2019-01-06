8998ff03fa1d6a2b3a7cc05e97dde28098b7b873
Adam Mertel <mertel.adam@gmail.com>
2019-01-06 21:11:30 +0100

    title and placeholder values as options (#16)

--- L.Control.Geonames.js
+++ L.Control.Geonames.js
@@ -15,25 +15,27 @@ L.Control.Geonames = L.Control.extend({
     _popup: null,
     _hasResults: false,
     options: {
-        //position: 'topcenter',  // in addition to standard 4 corner Leaflet control layout, this will position and size from top center
-        geonamesSearch: 'https://secure.geonames.org/searchJSON', // override this if using a proxy to get connection to geonames
-        geonamesPostalCodesSearch: 'https://secure.geonames.org/postalCodeSearchJSON', // override this if using a proxy to get connection to geonames
-        username: '', //Geonames account username.  Must be provided
-        maxresults: 5, //Maximum number of results to display per search
-        zoomLevel: null, //Max zoom level to zoom to for location.  If null, will use the map's max zoom level.
-        className: 'leaflet-geonames-icon', //class for icon
-        workingClass: 'leaflet-geonames-icon-working', //class for search underway
-        featureClasses: ['A', 'H', 'L', 'P', 'R', 'T', 'U', 'V'], //feature classes to search against.  See: http://www.geonames.org/export/codes.html
-        baseQuery: 'isNameRequired=true', //The core query sent to GeoNames, later combined with other parameters above
+        //position: 'topcenter', // In addition to standard 4 corner Leaflet control layout, this will position and size from top center.
         position: 'topleft',
-        showMarker: true, //Show a marker at the location the selected location
-        showPopup: true, //Show a tooltip at the selected location
-        adminCodes: {}, //Filter results by the specified admin codes mentioned in `ADMIN_CODES`. Each code can be a string or a function returning a string. `country` can be a comma-separated list of countries.
-        bbox: {}, //An object in form of {east:..., west:..., north:..., south:...}, specifying the bounding box to limit the results to
-        lang: 'en', //Locale of results
-        alwaysOpen: false, //if true, search field is always visible
-        enablePostalCodes: false, // if true, use postalCodesRegex to test user provided string for a postal code.  If matches, then search against postal codes API instead.
-        postalCodesRegex: POSTALCODE_REGEX_US // regex used for testing user provided string for a postal code.  If this test fails, the default geonames API is used instead.
+        geonamesSearch: 'https://secure.geonames.org/searchJSON', // Override this if using a proxy to get connection to geonames.
+        geonamesPostalCodesSearch: 'https://secure.geonames.org/postalCodeSearchJSON', // Override this if using a proxy to get connection to geonames.
+        username: '', // Geonames account username.  Must be provided.
+        maxresults: 5, // Maximum number of results to display per search.
+        zoomLevel: null, // Max zoom level to zoom to for location. If null, will use the map's max zoom level.
+        className: 'leaflet-geonames-icon', // Class for icon.
+        workingClass: 'leaflet-geonames-icon-working', // Class for search underway.
+        featureClasses: ['A', 'H', 'L', 'P', 'R', 'T', 'U', 'V'], // Feature classes to search against.  See: http://www.geonames.org/export/codes.html.
+        baseQuery: 'isNameRequired=true', // The core query sent to GeoNames, later combined with other parameters above.
+        showMarker: true, // Show a marker at the location the selected location.
+        showPopup: true, // Show a tooltip at the selected location.
+        adminCodes: {}, // Filter results by the specified admin codes mentioned in `ADMIN_CODES`. Each code can be a string or a function returning a string. `country` can be a comma-separated list of countries.
+        bbox: {}, // An object in form of {east:..., west:..., north:..., south:...}, specifying the bounding box to limit the results to.
+        lang: 'en', // Locale of results.
+        alwaysOpen: false, // If true, search field is always visible.
+        enablePostalCodes: true, // If true, use postalCodesRegex to test user provided string for a postal code.  If matches, then search against postal codes API instead.
+        postalCodesRegex: POSTALCODE_REGEX_US, // Regex used for testing user provided string for a postal code.  If this test fails, the default geonames API is used instead.
+        title: 'Search by location name or postcode', // Search input title value.
+        placeholder: 'Enter a location name' // Search input placeholder text.
     },
     onAdd: function (map) {
         if (this.options.position == 'topcenter') {
@@ -49,14 +51,14 @@ L.Control.Geonames = L.Control.extend({
 
         var link = this._link = L.DomUtil.create('a', this.options.className, this._container);
         link.href = '#';
-        link.title = 'Search by location name or postcode';
+        link.title = this.options.title;
 
         var form = L.DomUtil.create('form', '', this._container);
         L.DomEvent.addListener(form, 'submit', this._search, this);
 
         var input = this._input = L.DomUtil.create('input', '', form);
         input.type = 'search';
-        input.placeholder = 'Enter a location name' + (this.options.enablePostalCodes ? ' or postal codes' : '');
+        input.placeholder = this.options.placeholder;
 
         this._url = this.options.geonamesSearch;
         this._resultsList = L.DomUtil.create('ul', '', this._container);
@@ -168,7 +170,6 @@ L.Control.Geonames = L.Control.extend({
     _close: function () {
         // Clear search field (if not alwaysOpen, close results list, and
         // remove marker
-
         this.hide();
         this.removeMarker();
         this.removePopup();
@@ -186,8 +187,8 @@ L.Control.Geonames = L.Control.extend({
         }
     },
     _onPopupClosed: function () {
-        this._close();
-        this._input.value = ''
+        this.removeMarker();
+        this.removePopup();
         this._hasResults = false;
         this._resultsList.innerHTML = '';
     },
@@ -268,7 +269,7 @@ L.Control.Geonames = L.Control.extend({
         }
 
         var origScope = this;
-        var callbackName = 'geonamesSearchCallback';
+        var callbackName = 'geonamesSearchCallback' + new Date().getTime();
         this._jsonp(url,
             function (response) {
                 document.body.removeChild(document.getElementById('getJsonP'));
--- README.md
+++ README.md
@@ -38,27 +38,27 @@ Example usage:
 
 ```
 var control = L.control.geonames({
-    //position: 'topcenter',  // in addition to standard 4 corner Leaflet control layout, this will position and size from top center
-    geonamesURL: '//api.geonames.org/searchJSON',  // override this if using a proxy to get connection to geonames
-    username: '',  // Geonames account username.  Must be provided
-    zoomLevel: null,  // Max zoom level to zoom to for location.  If null, will use the map's max zoom level.
-    maxresults: 5,  // Maximum number of results to display per search
-    className: 'leaflet-geonames-icon', //class for icon
-    workingClass: 'leaflet-geonames-icon-working', //class for search underway
-    featureClasses: ['A', 'H', 'L', 'P', 'R', 'T', 'U', 'V'],  // feature classes to search against.  See: http://www.geonames.org/export/codes.html
-    baseQuery: 'isNameRequired=true',  // The core query sent to GeoNames, later combined with other parameters above
+    //position: 'topcenter', // In addition to standard 4 corner Leaflet control layout, this will position and size from top center.
     position: 'topleft',
-    showMarker: true, //Show a marker at the location the selected location
-    showPopup: true, //Show a tooltip at the selected location
-    adminCodes: { // filter results by a country and state.  Values can be strings or return by a function.
-        country: 'us',
-        adminCode1: function() {return 'wa'}
-    },
-    lang: 'en', // language for results
-    bbox: {east:-121, west: -123, north: 46, south: 45}, // bounding box filter for results (e.g., map extent).  Values can be an object with east, west, north, south, or a function that returns that object.
-    alwaysOpen: false,  //if true, search field is always visible
-    enablePostalCodes: false, // if true, use postalCodesRegex to test user provided string for a postal code.  If matches, then search against postal codes API instead.
-    postalCodesRegex: POSTALCODE_REGEX_US // regex used for testing user provided string for a postal code.  If this test fails, the default geonames API is used instead.
+    geonamesSearch: 'https://secure.geonames.org/searchJSON', // Override this if using a proxy to get connection to geonames.
+    geonamesPostalCodesSearch: 'https://secure.geonames.org/postalCodeSearchJSON', // Override this if using a proxy to get connection to geonames.
+    username: '', // Geonames account username.  Must be provided.
+    maxresults: 5, // Maximum number of results to display per search.
+    zoomLevel: null, // Max zoom level to zoom to for location. If null, will use the map's max zoom level.
+    className: 'leaflet-geonames-icon', // Class for icon.
+    workingClass: 'leaflet-geonames-icon-working', // Class for search underway.
+    featureClasses: ['A', 'H', 'L', 'P', 'R', 'T', 'U', 'V'], // Feature classes to search against.  See: http://www.geonames.org/export/codes.html.
+    baseQuery: 'isNameRequired=true', // The core query sent to GeoNames, later combined with other parameters above.
+    showMarker: true, // Show a marker at the location the selected location.
+    showPopup: true, // Show a tooltip at the selected location.
+    adminCodes: {}, // Filter results by the specified admin codes mentioned in `ADMIN_CODES`. Each code can be a string or a function returning a string. `country` can be a comma-separated list of countries.
+    bbox: {}, // An object in form of {east:..., west:..., north:..., south:...}, specifying the bounding box to limit the results to.
+    lang: 'en', // Locale of results.
+    alwaysOpen: false, // If true, search field is always visible.
+    enablePostalCodes: true, // If true, use postalCodesRegex to test user provided string for a postal code.  If matches, then search against postal codes API instead.
+    postalCodesRegex: POSTALCODE_REGEX_US, // Regex used for testing user provided string for a postal code.  If this test fails, the default geonames API is used instead.
+    title: 'Search by location name or postcode', // Search input title value.
+    placeholder: 'Enter a location name' // Search input placeholder text.
 });
 map.addControl(control);
 ```
